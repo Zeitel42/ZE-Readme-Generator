d5d85aec17dd6f06720e0044f1dc777012664c32
Zeitel42 <43343991+Zeitel42@users.noreply.github.com>
2022-04-07 18:29:59 -0400

    tested writeFile with one question/answer success

---
+++ Develop/index.js
@@ -0,0 +1,63 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+
+const fs = require('fs');
+
+const generatePage = require('./utils/generateMarkdown');
+
+// // TODO: Create an array of questions for user input
+
+const questions = () => {
+    return inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?(Required)',
+            validate: nameInput => {
+              if (nameInput) {
+                return true;
+              } else {
+                console.log("please enter your project's title!");
+                return false;
+              }
+            }
+          }
+          
+    ])
+};
+questions()
+// .then((answers => {
+//     console.log(answers);
+//     console.log('#', answers.title);
+// }))
+.then(answers => {
+      const pageHTML = generatePage(answers); 
+      fs.writeFile('./utils/README.md', pageHTML, err => {
+              if (err) throw new Error(err);
+        
+              console.log('Page created! Check out index.html in this directory to see it!');
+            });
+          });
+
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+//////////////////////  MOCK DATA (don't forget to comment out) ////////////////////
+// const mockData = {
+//     title: 'balls',
+//     link: 'ballshub.io',
+//     description: 'has all the balls of all the balls',
+//     contribution: ['some balls', 'other balls']
+//   }
+//   const pageHTML = generatePage(mockData);
+//   fs.writeFile('./utils/README.md', pageHTML, err => {
+//     if (err) throw new Error(err);
+//   });
+//     console.log('Page created! Check out index.html in this directory to see it!');
+  //////////////////////////////////////////////////////////////////
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
---
+++ Develop/utils/README.md
@@ -0,0 +1,2 @@
+# Not Balls
+
